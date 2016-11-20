4380164f8b3830f69e2e9218983315abc65d0ad3
Anton Vaneev <yoda@Antons-MacBook-Pro.local>
2016-11-20 15:35:21 -0500

    [WE] Initial commit - created an empty project, added basic files

---
+++ WorkflowEssentials/WorkflowEssentials.h
@@ -0,0 +1,21 @@
+//
+//  WorkflowEssentials.h
+//  Workflow Essentials
+//
+//  Created by Anton Vaneev.
+//  Copyright (c) 2016-present, Anton Vaneev. All rights reserved.
+//
+//  Distributed under BSD license. See LICENSE for details.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkflowEssentials.
+FOUNDATION_EXPORT double WorkflowEssentialsVersionNumber;
+
+//! Project version string for WorkflowEssentials.
+FOUNDATION_EXPORT const unsigned char WorkflowEssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkflowEssentials/PublicHeader.h>
+
+
