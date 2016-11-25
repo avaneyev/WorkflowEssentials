3102f0a10ed15fe5f973403cc2a097674753bdb9
Anton Vaneev <avaneyev@gmail.com>
2016-11-24 23:28:23 -0500

    Clarify the comments a bit

--- WorkflowEssentials/Operation/WEOperation.h
+++ WorkflowEssentials/Operation/WEOperation.h
@@ -16,17 +16,27 @@
 
 - (nonnull instancetype)initWithName:(nullable NSString *)name NS_DESIGNATED_INITIALIZER;
 
-// Optional operation name that can be used by other operations to its result or
-// the operation itself as a dependency.
+/**
+ Optional operation name that can be used by other operations to its result or
+ the operation itself as a dependency.
+ */
 @property (nonatomic, readonly, retain, nullable) NSString *name;
 
+/**
+ An operation should return YES if it requires main thread for execution, otherwise it should return NO.
+ Operations that require main thread will be performed on the main queue, other operations will be
+ dispatched to background queues based on other preferences.
+ NOTE that unlike with dispatch/operation queues, workflow may execute more than one operation that
+ requires main queue if they are async and don't depend on each other.
+ */
 @property (nonatomic, readonly) BOOL requiresMainThread;
 
 /**
  Called when the workflow is ready to start an operation, but before the start.
  Allows an operation to to prepare itself for execution.
  This gives an operation a chance to check its prerequisites and schedule additional work to be done 
- before the operation is performed
+ before the operation is performed.
+ Default implementation does nothing.
  */
 - (void)prepareForExecution;
 
